172e8ee0125055a344a8aa3d51622b3d5b903dc1
Nina <napril96@gmail.com>
2018-12-13 12:52:23 +0200

    fix new email validation - check admins also

--- src/main/java/com/chernivtsi/doctorsoffice/constraint/validator/EmailExistValidator.java
+++ src/main/java/com/chernivtsi/doctorsoffice/constraint/validator/EmailExistValidator.java
@@ -1,6 +1,7 @@
 package com.chernivtsi.doctorsoffice.constraint.validator;
 
 import com.chernivtsi.doctorsoffice.constraint.EmailExistConstraint;
+import com.chernivtsi.doctorsoffice.service.AdminService;
 import com.chernivtsi.doctorsoffice.service.UserService;
 
 import javax.validation.ConstraintValidator;
@@ -13,9 +14,11 @@ public class EmailExistValidator implements ConstraintValidator<EmailExistConstr
 
     private String message;
     private UserService userService;
+    private AdminService adminService;
 
-    public EmailExistValidator(UserService userService) {
+    public EmailExistValidator(UserService userService, AdminService adminService) {
         this.userService = userService;
+        this.adminService = adminService;
     }
 
     @Override
@@ -27,7 +30,8 @@ public class EmailExistValidator implements ConstraintValidator<EmailExistConstr
     public boolean isValid(String mail, ConstraintValidatorContext constraintValidatorContext) {
 
 
-        boolean isValid = !userService.findUserByEmail(mail).isPresent();
+        boolean isValid = !userService.findUserByEmail(mail).isPresent()
+                && !adminService.findAdminByEmail(mail).isPresent();
 
         if (isValid) {
             constraintValidatorContext.buildConstraintViolationWithTemplate(message)
