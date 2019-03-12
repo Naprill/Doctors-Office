6bea2114c1bb474ae14f0d2fc1ef8aa9ab1c56fc
Nina <napril96@gmail.com>
2019-03-12 14:49:25 +0200

    add date to feedback

--- src/main/java/com/chernivtsi/doctorsoffice/model/Feedback.java
+++ src/main/java/com/chernivtsi/doctorsoffice/model/Feedback.java
@@ -3,9 +3,12 @@ package com.chernivtsi.doctorsoffice.model;
 import com.chernivtsi.doctorsoffice.model.base.AbstractIdentifiable;
 import lombok.Getter;
 import lombok.ToString;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -16,4 +19,7 @@ public class Feedback extends AbstractIdentifiable {
 
 	@Column(columnDefinition = "text", length = 10485760)
 	private String text;
+
+	@Convert(converter = Jsr310JpaConverters.LocalDateConverter.class)
+	private LocalDate date;
 }
